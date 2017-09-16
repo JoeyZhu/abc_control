7f5637c2b74aa30d60cea89f2497e565b5b1deb0
JoeyZhu <zhu1ying3@gmail.com>
2017-09-16 08:17:25 +0800

    init commit

---
+++ src/abc_control_node.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep 15, 2017
+ *      Author: joey
+ */
+
+
+
+#include <ros/ros.h>
+
+
+int main(int argc, char** argv){
+	ROS_INFO("hello from abc_control!");
+}
