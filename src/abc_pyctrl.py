60523375144c81248b17faca40546fc746d33737
JoeyZhu <zhu1ying3@gmail.com>
2017-09-17 15:42:11 +0800

    framework ready

--- src/abc_pyctrl.py
+++ src/abc_pyctrl.py
@@ -2,20 +2,32 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
-def abc_control():
-    rospy.init_node('abc_pyctrl', anonymous=True)
-    pub = rospy.Publisher('test', String, queue_size=10)
+class ABCNode:
     
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
-
+    def __init__(self):
+        rospy.init_node('ABC')
+        rospy.loginfo("ABC init")
+        rospy.Subscriber("/cmd_vel", Twist, self.cmdVelCb)
+        self.cmd_vel = [0, 0]
+        
+    def spin(self):
+        r = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            rospy.loginfo_throttle(1, "abc running")
+            r.sleep()
+            
+    def cmdVelCb(self, req):
+        x = req.linear.x
+        th = req.angular.z
+        ## send cmd to gpio here
+        logstr =  "get x, z:" + str(x) + ", " + str(th)
+        rospy.loginfo_throttle(1, logstr)
+            
 if __name__ == '__main__':
     try:
-        abc_control()
+        robot = ABCNode()
+        robot.spin()
     except rospy.ROSInterruptException:
         pass
